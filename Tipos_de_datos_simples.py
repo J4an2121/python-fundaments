fcc74f35b4ebc9a487d8a8b0b8bf5cfd97bb8652
Juan Esteban David cano <149740237+J4an2121@users.noreply.github.com>
2025-10-31 09:46:47 -0500

    Update Tipos_de_datos_simples.py

--- Tipos_de_datos_simples.py
+++ Tipos_de_datos_simples.py
@@ -1,3 +1,34 @@
+ipos de datos simples en Python
+
+En Python, los tipos de datos simples son los que se usan para representar valores básicos como texto o números.
+Los más comunes son:
+
+*Un string es una secuencia de caracteres: letras, números o símbolos.
+Siempre se escriben entre comillas simples (' '), dobles (" ") o triple comillas (''' ''' o """ """).
+
+*Números
+
+Python maneja dos tipos principales de números simples:
+Enteros (int)
+Son números sin decimales.
+
+b) Flotantes (float)
+
+Son números con decimales
+
+* Operaciones matemáticas básicas
+
+Podemos realizar operaciones con números como si fuera una calculadora:
+
+print(10 + 5)   # Suma
+print(10 - 5)   # Resta
+print(10 * 5)   # Multiplicación
+print(10 / 5)   # División con decimal
+print(10 // 5)  # División entera (sin decimales)
+
+
+String (Cadenas de texto)
+
 #string
 
 print("Esto es un string")
@@ -27,5 +58,6 @@ print(10 // 5)  # Division entera
 #sintanxis 
 
 print(f"cadena de texto combinada {10 + 5}")
+
 
 print(f"el numero 10.5 tiene el tipo de dato {type(10.5)}")
